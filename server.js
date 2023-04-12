dae3c527048ea9bbafb93a778dc1751b1f623805
Matthew Baty <mattbaty@outlook.com>
2023-04-12 19:26:53 -0400

    server.js file done, commented out api routes unitl they are set up for testing

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const PORT = process.env.PORT || 3001;
+// packages
 const express = require('express');
-const apiRoutes =require('./routes/api');
-const htmlRoutes = require('./routes/html');
+// const apiRoutes =require('./routes/api');
+// const htmlRoutes = require('./routes/html');
 
+// port
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+// express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+// listen to the server
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
