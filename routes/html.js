94542313e22cf13b9ad2423196b26b025ba87c9f
Matthew Baty <mattbaty@outlook.com>
2023-04-14 18:34:26 -0400

    html routes done

---
+++ routes/html.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const router = require('express').Router();
+
+// get route homepage
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+// get route notes page
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+// get * route homepage
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+// export the router 
+module.exports = router;
